54b41452857fac4e65caefc64c546906fdd41884
salaverryy <andres.2005.fut@gmail.com>
2025-02-07 12:03:56 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,34 +1,58 @@
-const app = require('../app.js');
-const request = require('supertest')(app);
+const express = require('express');
+const _ = require('underscore');
 
-describe('GET', function(){
-	  it('respuesta contiene text/html', function(done){
-		      request
-		      .get('/')
-		      .set('Accept', 'text/html')
-		      .expect('Content-Type', /html/)
-		      .expect(200, done);
-		    })
+const port = process.env.PORT || 3000;
+const animals = {
+    "cat": "meow",
+    "dog": "bark",
+    "eel": "hiss",
+    "bear": "growl",
+    "frog": "croak",
+    "lion": "roar",
+    "bird": "tweet",
+    "fish": "gluuu"
+};
 
-	  it('respuesta contiene George Orwell', function(done){
-		      request
-		      .get('/')
-		      .set('Accept', 'text/html')
-		      .expect(200, /George Orwell had a farm/ig, done);
-		    })
+function getAnimal() {
+  return _.sample(Object.entries(animals));
+}
 
-	  it('/api respuesta contiene json', function(done){
-		      request
-		      .get('/api')
-		      .set('Accept', 'application/json')
-		      .expect('Content-Type', /json/)
-		      .expect(200, done);
-		    })
+const app = express();
 
-	  it('/api respuesta contiene objeto animales', function(done){
-		      request
-		      .get('/api')
-		      .set('Accept', 'application/json')
-		      .expect(200, {"cat":"meow","dog":"bark","eel":"hiss","bear":"growl","frog":"croak","lion":"roar","bird":"tweet"}, done);
-		    })
-})
+app.get('/', async (req, res, next) => {
+  try {
+    const [animal_name, sound] = getAnimal();
+    res.status(200).send(`
+      George Orwell had a farm.<br />
+      E-I-E-I-O<br />
+      And on his farm he had a ${animal_name}.<br />
+      E-I-E-I-O<br />
+      With a ${sound}-${sound} here.<br />
+      And a ${sound}-${sound} there.<br />
+      Here a ${sound}, there a ${sound}.<br />
+      Everywhere a ${sound}-${sound}.<br />
+    `);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api', async (req, res, next) => {
+  try {
+    res.status(200).json(animals);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Launching server on http://localhost:${port}`);
+});
+
+module.exports = server; 
